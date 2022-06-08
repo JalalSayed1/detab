9d9c2d4fe97237b53ce02020eb1136fa3a680c71
Jalal Sayed <mrjalalsayed@gmail.com>
2022-06-08 21:48:30 +0100

    Buggy code

--- detab.c
+++ detab.c
@@ -2,32 +2,105 @@
 
 #include <stdio.h>
 
-void replacetabs();
+#define TABSTOP 8
+#define MAXLEN 1000
+
+void replacetabs(int len);
+int mygetline(void);
+void putline(void);
+
+char line[MAXLEN];
 
 int main()
 {
+    extern char line[];
+    int len;
+    // int ncolumns = 0;
 
-    char c;
-
-    while ((c = getchar()) != EOF)
+    while ((len = mygetline()) > 0)
     {
-        if (c == '\t')
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                replacetabs();
-            }
-        }
-        else
-        {
-            putchar(c);
-        }
+        replacetabs(len);
+        putline();
     }
 
     return 0;
 }
 
-void replacetabs()
+void replacetabs(int len)
 {
-    putchar(' ');
+    extern char line[];
+
+    int i;
+    int ncolumns = 0;
+
+    for (i = 0; i < len; ++i)
+    {
+        ++ncolumns;
+        if (ncolumns == TABSTOP)
+        {
+            ncolumns = 0;
+        }
+
+        if (line[i] == '\t')
+        {
+            int pos = i;
+            for (int j = 0; j < (TABSTOP - ncolumns); ++j)
+            {
+                ++len;
+                // shift elts:
+                int z;
+                for (z = len-1; z < i; --z)
+                {
+                    line[z] = line[z - 1];
+                }
+
+                line[i] = '_';
+            }
+            
+            // line[i] = ' ';
+
+            // total spaces required = spacesrequired + prev space added:
+            // int spacesrequired = TABSTOP - ncolumns;
+            // printf("spaces req: %d\n", spacesrequired);
+            // char temp;
+
+            // for (int z = 0; z < spacesrequired; ++z)
+            // {
+            //     // -1 is \0, and -2 is \n:
+            //     temp = line[len - 3];
+            //     for (int j = len - 3; j > 0; --j)
+            //     {
+            //         // not checking for overflow!
+            //         line[j] = line[j - 1];
+            //     }
+            //     line[len+z-1] = temp;
+            // }
+        }
+    }
+}
+
+int mygetline(void)
+{
+    extern char line[];
+
+    int c, i;
+
+    for (i = 0; (i < MAXLEN - 1) && ((c = getchar()) != EOF) && c != '\n'; ++i)
+    {
+        line[i] = c;
+    }
+
+    if (c = '\n')
+    {
+        line[i] = c;
+        ++i;
+    }
+
+    line[i] = '\0';
+
+    return i;
+}
+
+void putline(void){
+    printf("%s", line);
 }
