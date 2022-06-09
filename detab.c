1c460b3e94e45fb3dee9228a39a1ed2d7aeb16a1
Jalal Sayed <mrjalalsayed@gmail.com>
2022-06-09 20:30:14 +0100

    Fixed not terminating at EOF bug

--- detab.c
+++ detab.c
@@ -15,7 +15,6 @@ int main()
 {
     extern char line[];
     int len;
-    // int ncolumns = 0;
 
     while ((len = mygetline()) > 0)
     {
@@ -26,15 +25,17 @@ int main()
     return 0;
 }
 
+/**
+ * @brief Replace every tab character with the corresponding number of spaces.
+ *
+ * @param len length of line input.
+ */
 void replacetabs(int len)
 {
     extern char line[];
 
     int i;
     int ncolumns = 0;
-
-    // without \n:
-    --len;
 
     for (i = 0; i < len; ++i)
     {
@@ -46,17 +47,17 @@ void replacetabs(int len)
 
         if (line[i] == '\t')
         {
-            // without \n:
-            // --len;
+
             int spacesneeded = (TABSTOP - ncolumns);
             int pos = i;
 
             while (spacesneeded > 0)
             {
-                line[pos] = ' ';
+                line[pos] = '-';
 
                 for (int j = len - 1; j > i; --j)
                 {
+                    //! no overflow checking - MAXLEN:
                     line[j + 1] = line[j];
                 }
 
@@ -70,16 +71,16 @@ void replacetabs(int len)
 
 /**
  * @brief get a line of input, defined by a newline.
- * 
+ *
  * @return int length of input line.
  */
 int mygetline(void)
 {
-    extern char line[];
-
     int c, i;
 
-    for (i = 0; (i < MAXLEN - 1) && ((c = getchar()) != EOF) && c != '\n'; ++i)
+    extern char line[];
+
+    for (i = 0; (i < MAXLEN - 1) && ((c = getchar()) != EOF) && (c != '\n'); ++i)
     {
         line[i] = c;
     }
@@ -87,10 +88,10 @@ int mygetline(void)
     if (c = '\n')
     {
         line[i] = c;
-        ++i;
+        // ++i;
     }
 
-    line[i] = '\0';
+    line[i + 1] = '\0';
 
     return i;
 }
